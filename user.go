6a68f1a7ef7f778bc26971f7fc7c96791065a82f
Jordan Sheehan <jordansheehan@ufl.edu>
2023-01-18 20:19:02 -0500

    Added update and delete handlers.

--- user.go
+++ user.go
@@ -61,9 +61,19 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.First(&user, params["id"])
+	json.NewDecoder(r.Body).Decode(&user)
+	DB.Save(&user)	
+	json.NewEncoder(w).Encode(user)
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.Delete(&user, params["id"])
+	json.NewEncoder(w).Encode("The user has been deleted successfully.")
 }
